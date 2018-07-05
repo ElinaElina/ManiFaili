8210c3356d42711c470cb564c721dd55d7a82737
RigaCodingaSchool <40607133+ElinaElina@users.noreply.github.com>
2018-07-05 18:33:15 +0300

    Pabeigta diena 8

---
+++ day8/day8/Cikluuzdevumi.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day8
+{
+    class Cikluuzdevumi
+    {
+        public void Zvaigznite()
+        {
+            // cik reizes iet cauri cilveks ar Console.Readline
+            // cik cilveks ievada skaitli tik lai izvadas zvaigznites un lai radas egliitee
+            //parbaudam kluda un izvadam kludas pazinojumu ja ievaditais skitlis ir 0 vai negativs, ciklu var izmantot jebkuru no 3jiem
+
+            Console.WriteLine("Ievadi skaitli");
+            string input = Console.ReadLine();
+            int robeza = Convert.ToInt16(input);
+
+            string zvaigznite = "";
+
+            for (int i = 0; i < robeza; i++)
+            {
+                zvaigznite = zvaigznite + "*";
+                Console.WriteLine(zvaigznite); // sadi zvaigznite izvadas eglite, cikla neko neparbauda
+
+            }
+            Console.ReadLine();
+
+            if (robeza <= 0)
+            {
+                Console.WriteLine("Kludaina ievade");
+            }
+            else
+            {
+                for (int i = 0; i < robeza; i++)
+                {
+                    zvaigznite = zvaigznite + "*";
+                    Console.WriteLine(zvaigznite);
+                }
+            }
+            int skaititajs = 0;
+            zvaigznite = "";
+            while (skaititajs != robeza)
+            {
+                zvaigznite = zvaigznite + "*";
+                Console.WriteLine(zvaigznite);
+                skaititajs++;     // NEAIZMIRST !!!!nedrikst so aizmirst jo citaadi ciklaa ies ieksaa muziigi, so parasti, lietojot while, aizmirst!!!!
+            }
+
+        }
+
+    }
+}
---
+++ day8/day8/Masivi.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day8
+{
+    class Masivi
+    {
+        public void IzvaditMasivu()
+        {
+            int[] piemeraMasivs = new int[5];              //jauztaisa objekts, skaitlis iekavas - cik masiva var glabaties vertibas, solu skaits
+
+            piemeraMasivs[0] = 4;                         // pieskirt vertibu, iekavas norada ,kuram solim pieklut grib, masiviem ir pirmais elements 0, obligati pirmais elements ir 0
+            piemeraMasivs[1] = 99;
+            piemeraMasivs[2] = 9;
+            piemeraMasivs[3] = 6567;
+            piemeraMasivs[4] = 567;                      // jo pieci soli ,ko noradija sakuma int iekavas
+            Console.WriteLine(piemeraMasivs[0]);        // iekavas raksta to kuru izvadit, ja liks 3 tad izvadisies 6567
+
+            for (int i = 0; i < 5; i++)                // visertaks izsaukt ar ciklu
+            {
+                Console.WriteLine(piemeraMasivs[i]);   // masivi ir ertakais kaa glabat datus un kaa pieklut katram elementam
+            }
+
+            string[] stringMasivs = new string[5];
+
+            /*stringMasivs[0] = "*";
+            stringMasivs[1] = "**";
+            stringMasivs[2] = "***";
+            stringMasivs[3] = "****";
+            stringMasivs[4] = "*****";*/
+
+            
+
+
+
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(piemeraMasivs[i]);
+            }
+
+            string abs = stringMasivs[3];    // masivs ar elemntu 3
+            string summa = "";
+            for(int i = 0; i < 5; i++)
+            {
+                summa = summa + "*";
+                stringMasivs[1] = summa;
+
+            }
+            stringMasivs[1] = summa;
+            for(int i=0; i < 5; i++)
+            {
+                Console.WriteLine(piemeraMasivs[i]);
+            }
+            for(int i=4; i >= 0; i--)
+            {
+                Console.WriteLine(piemeraMasivs[i]);
+            }
+            for(int i = 0; i <= 0; i++)
+            {
+                
+                Console.WriteLine(piemeraMasivs[i]);
+            }
+        }   
+
+
+    }
+}
